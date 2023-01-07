e2b626a8c03328cf5629ab676f7acec56ef08038
Aditya Lahane <adityalahane31@gmail.com>
2023-01-07 11:39:31 +0530

    Updated model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 app=Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model1.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
